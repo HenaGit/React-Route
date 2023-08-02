6c76f1e47e825cf3947f82c1fdafb8dee227bcf4
HenokG <heni.gebrehiwot@gmail.com>
2023-08-02 15:21:08 -0600

    Parameters in URL

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import ProductList from "./Pages/ProductList";
 import ProductDetails from "./Pages/ProductDetails";
 import Product from "./Pages/Product";
 import NotFound from "./NotFound";
+import CryptoDetail from "./CryptoDetail";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -18,6 +20,14 @@ root.render(
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
+        <Route
+          path="/cryptodetail/:cryptoSymbol/:id"
+          element={<CryptoDetail />}
+        ></Route>
+        <Route
+          path="/cryptodetail/:cryptoSymbol"
+          element={<CryptoDetail />}
+        ></Route>
         <Route path="product">
           <Route path="" element={<Product />}></Route>
           <Route path="create" element={<CreateProduct />}></Route>
